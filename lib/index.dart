1109196960cc8c321c43ba688b5944dc563cc5cf
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-10-28 18:17:56 +0100

    added pages

--- lib/index.dart
+++ lib/index.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:modern_food_app/features/details/screen/details_screen.dart';
+import 'package:modern_food_app/features/home/screen/home_screen.dart';
 
-class Index extends StatelessWidget {
+class Index extends StatefulWidget {
   const Index({super.key});
 
   @override
+  State<Index> createState() => _IndexState();
+}
+
+class _IndexState extends State<Index> {
+  int currentIndex = 0;
+  List<Widget> pages = [
+    HomeScreen(),
+    DetailsScreen(),
+    HomeScreen(),
+    DetailsScreen(),
+    HomeScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(index: currentIndex, children: pages),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        elevation: 0,
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 20,
+
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        ],
+      ),
+    );
   }
 }
