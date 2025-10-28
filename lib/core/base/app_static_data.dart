e9125fa817bfa4475a71a1c39289fc66c856d35c
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-10-28 12:02:40 +0100

    static files for selecting food categories

---
+++ lib/core/base/app_static_data.dart
@@ -0,0 +1,22 @@
+class AppStaticData {
+  static List<Map<String, dynamic>> productList = [
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+    {"title" : "Pizza",
+      "image" : "assets/images/squirrel-619968_1280.jpg"
+    },
+  ];
+}
