90931a0eaf0dbfe1900ff2171f1be55d3ae8e29e
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-11-08 11:11:36 +0100

    favorite screen completed

--- lib/features/favorite/component/favorite_card.dart
+++ lib/features/favorite/component/favorite_card.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class FavoriteCard extends StatelessWidget {
-  const FavoriteCard({super.key});
+  const FavoriteCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.price,
+  });
+
+  final String image;
+  final String title;
+  final String subtitle;
+  final String price;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.grey.shade300
+      ),
+      child: Row(
+        spacing: 10,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
+            child: Image.asset(
+              image,
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
+              ),
+          ),
+          Column(
+            spacing: 5,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text(title, style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.bold
+            ),), Text(subtitle), Text('\$${price.toString()}')]),
+        ],
+      ),
+    );
   }
 }
--- lib/features/favorite/screen/favorite_screen.dart
+++ lib/features/favorite/screen/favorite_screen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:modern_food_app/core/base/app_static_data.dart';
+import 'package:modern_food_app/features/favorite/component/favorite_card.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appData = AppStaticData().cartData;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorite Screen'),
+      appBar: AppBar(title: const Text('Favorite Screen', style: TextStyle(fontSize: 18),),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Icon(Icons.favorite),
+        )
+      ],
       ),
-      body: const Center(
-        child: Text('This is the Favorite Screen'),
+      body: ListView.builder(
+        itemCount: appData.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 5),
+            child: FavoriteCard(
+              image: appData[index]['image'],
+              title: appData[index]['title'],
+              subtitle: appData[index]['subTitle'],
+              price: appData[index]['price'],
+            ),
+          );
+        },
       ),
     );
   }
