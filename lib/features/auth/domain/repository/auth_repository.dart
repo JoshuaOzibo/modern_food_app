e55d3a7099c130e88484460d7e582f8d5eb79bf3
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-11-10 08:05:43 +0100

    added repository for auth

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:modern_food_app/core/error/failure.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signupWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> signinWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+}
