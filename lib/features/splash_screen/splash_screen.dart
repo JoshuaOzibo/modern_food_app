d60de5663c6e6bef02301ae28db0b0a6f2dacb48
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-11-10 16:47:51 +0100

    feat: enhance SplashScreen with animation and layout adjustments

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -6,64 +7,126 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late Animation<AlignmentGeometry> smallCircleTransition;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    smallCircleTransition = Tween<AlignmentGeometry>(
+      begin: Alignment(0, 0),
+      end: Alignment(50, 0),
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Row(
-            children: [
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    height: 30,
-                    width: 30,
-                    margin: const EdgeInsets.only(left: 20),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.deepOrange,
-                    ),
-                  ),
-                ),
-              ),
-
-              Expanded(
-                child: Center(
-                  child: Stack(
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: AlignTransition(
+                      alignment: smallCircleTransition,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        margin: const EdgeInsets.only(left: 20, bottom: 80),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
                           color: Colors.deepOrange,
                         ),
                       ),
-                      Positioned(
-                        right: 35,
-                        bottom: 35,
-                        child: Container(
-                          height: 30,
-                          width: 30,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.deepOrange,
+                            ),
+                          ),
+                          Positioned(
+                            right: 35,
+                            bottom: 35,
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      FittedBox(
+                        fit: BoxFit.none,
+                        child: RichText(
+                          text: TextSpan(
+                            text: 'Foodie ',
+                            style: TextStyle(
+                              color: Colors.deepOrange,
+                              fontSize: 45,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'App',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 45,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      FittedBox(
+                        fit: BoxFit.none,
+                        child: Text(
+                          'Discover delicious meals delivered fresh to your door',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey.shade500,
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-              const Expanded(child: SizedBox()),
-            ],
-          ),
+                const Expanded(child: SizedBox()),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-
